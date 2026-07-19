4af3874f52deff80f98d7763f9bc89abd99f3cd3
brunoverly <bsverly@gmai.com>
2026-07-19 16:15:30 -0300

    adicionado o model foods

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Food extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'protein_g',
+        'carbohydrate_g',
+        'fat_g',
+        'calories_kcal',
+        'source',
+    ];
 }
